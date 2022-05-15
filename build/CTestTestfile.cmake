236f2cf8c41c5406213b337ebb4c1f3bc3973bb3
Filipe <69159895+Curvu@users.noreply.github.com>
2022-05-15 14:46:13 +0100

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/Utilizador/Documents/GitHub/Trabalho_PPP
-# Build directory: /mnt/c/Users/Utilizador/Documents/GitHub/Trabalho_PPP/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
