5fde1384956a0759f8e8a6f85a54dc6b90672d6c
Filipe Rodrigues <rodrigues.filipealexandre2003@gmail.com>
2022-05-30 18:09:49 +0100

    Update main.c
    Verificar que a lista não é null
    Dar include

--- main.c
+++ main.c
@@ -1,15 +1,19 @@
 #include "./headers/principal.h"
 #include "./headers/auxiliar.h"
+#include "./headers/alunos.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 int main() {
   lista l;
-  inicializa_lista(&l);
+  if (inicializa_lista(&l) == 0) {
+    printf("Erro ao inicializar a lista\n");
+    return 0;
+  }
 
   clean();
 
   while (menu(&l));
 
-return 0;
+  return 0;
 }
